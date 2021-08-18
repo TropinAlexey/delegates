6a6f9dc25b3145f5ddd2bc87f2cd24eb66a2c57a
Tropin Alexey <a.tropin@lakmus/org>
2021-08-18 17:25:17 +0300

    _6_DefineOperationsCalculator

---
+++ 6 DefineOperationsCalculator/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Collections.Generic;
+
+namespace _6_DefineOperationsCalculator
+{
+    class Program
+    {
+        private static readonly Dictionary<string, Func<double, double, double>> _operations = new Dictionary<string, Func<double, double, double>>
+        {
+            {"+", (x, y) => x + y},
+            {"-", (x, y) => x - y},
+            {"*", (x, y) => x * y},
+            {"/", (x, y) => x / y}
+        };
+
+        public static double PerformOperation(string operation, double x, double y)
+        {
+            if (!_operations.ContainsKey(operation))
+                throw new ArgumentException($"Operation {operation} is invalid", nameof(operation));
+            return _operations[operation](x, y);
+        }
+
+        public static void DefineOperation(string operation, Func<double, double, double> body)
+        {
+            if (_operations.ContainsKey(operation))
+                throw new ArgumentException($"Operation {operation} already exists", nameof(operation));
+            _operations.Add(operation, body);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first operand:");
+                var x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter operation:");
+                var op = Console.ReadLine();
+
+                if (op?.ToLower() == "define")
+                {
+                    Console.WriteLine("Enter new operation name:");
+                    var operationName = Console.ReadLine();
+                    Console.WriteLine("Define operation:");
+                    var body = Console.ReadLine();
+                    DefineOperation(operationName, CSharpScript.EvaluateAsync<Func<double, double, double>>(body).GetAwaiter().GetResult());
+
+                    Console.WriteLine("Enter operation:");
+                    var op2 = Console.ReadLine();
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter second operand:");
+                    var y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Result is: {PerformOperation(op2, x, y)}");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter second operand:");
+                    var y = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine($"Result is: {PerformOperation(op, x, y)}");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ LambdaFunctionsCalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaFunctionsCalculator
+{
+    class Program
+    {
+        private static readonly Dictionary<string, Func<double, double, double>> _operations = new Dictionary<string, Func<double, double, double>>
+        {
+            {"+", (x, y) => x + y},
+            {"-", (x, y) => x - y},
+            {"*", (x, y) => x * y},
+            {"/", (x, y) => x / y}
+        };
+
+        public static double PerformOperation(string operation, double x, double y)
+        {
+            if (!_operations.ContainsKey(operation))
+                throw new ArgumentException($"Operation {operation} is invalid", nameof(operation));
+            return _operations[operation](x, y);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first operand:");
+                var x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter operation:");
+                var op = Console.ReadKey().KeyChar.ToString();
+                Console.WriteLine();
+
+                Console.WriteLine("Enter second operand:");
+                var y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Result is: {PerformOperation(op, x, y)}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
