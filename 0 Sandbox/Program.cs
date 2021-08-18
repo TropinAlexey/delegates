da7c5e8aee51f6b82fc0e8bea6731e460dc8184b
Tropin Alexey <a.tropin@lakmus/org>
2021-08-18 18:00:04 +0300

    + sandbox

---
+++ 0 Sandbox/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public delegate void Del(string message);
+
+namespace _0_Sandbox
+{
+    class Program
+    {
+        // Create a method for a delegate.
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            // Instantiate the delegate.
+            Del handler = DelegateMethod;
+
+            // Call the delegate.
+            handler("Hello World");
+        }
+    }
+}
