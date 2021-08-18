ba8bcfd9be73bf92e1720fa16d16ed2549a37759
Tropin Alexey <a.tropin@lakmus/org>
2021-08-18 15:39:19 +0300

    init

---
+++ SwitchCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
